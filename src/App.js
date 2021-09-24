7329ed1a03f9675cad5d734ac243c02f5fc65859
Steven Rico <steven.rico.dev@gmail.com>
2021-09-24 23:16:41 +0100

    Feat: Setup react-router-dom.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import "./styles/reset.css"
 import "./styles/index.css"
+import { Route, Switch } from "react-router-dom"
 
 function App() {
-  return <h1>Hello World!</h1>
+  return (
+    <Switch>
+      <Route path="/exhibitions">
+        <h1>Exhibitions</h1>
+      </Route>
+      <Route path="/">
+        <h1>Home</h1>
+      </Route>
+    </Switch>
+  )
 }
 
 export default App
